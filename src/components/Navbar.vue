d7a05074e744d769fb32f65f7c1244f0e46e7874
hern <hernfhaja@gmail.com>
2021-10-02 16:25:16 +0700

    initial

---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+<div>hello from navbar</div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data () {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
